11315dbf2cf63f70c051fc87ed01f42b8f51a4f8
kouki <kouworld0123@gmail.com>
2017-12-14 04:23:40 +0900

    いい感じに実装 not debug not tit42tat

--- 2/fullplayer.c
+++ 2/fullplayer.c
@@ -21,59 +21,114 @@
 #define DECEPTION 0
 #endif
 
-
-void Player1 (int previousPlayer1Strategy, int previousPlayer2Strategy, int *currentPlayer1Strategy)
+void Player1(int previousPlayer1Strategy, int previousPlayer2Strategy, int *currentPlayer1Strategy)
 {
     return;
 }
-void Player2 (int previousPlayer1Strategy, int previousPlayer2Strategy, int *currentPlayer2Strategy)
+void Player2(int previousPlayer1Strategy, int previousPlayer2Strategy, int *currentPlayer2Strategy)
 {
     return;
 }
 
-void tit4tat (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+void tit4tat(int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
 {
     if (previousPlayerStrategy == -1 && previousPartnerStrategy == -1)
     {
         *currentPlayerStrategy = COOPERATION;
-    }else
+    }
+    else
     {
         *currentPlayerStrategy = previousPartnerStrategy;
     }
     return;
 }
 
-void rtit4tat (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+void rtit4tat(int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
 {
+    if (previousPlayerStrategy == -1 && previousPartnerStrategy == -1)
+    {
+        *currentPlayerStrategy = DECEPTION;
+    }
+    else
+    {
+        *currentPlayerStrategy = previousPartnerStrategy;
+    }
     return;
 }
 
-void tit42tat (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+void tit42tat(int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
 {
+
     return;
 }
 
-void friedman (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+void friedman(int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
 {
+    *currentPlayerStrategy = COOPERATION;
+    if (previousPlayerStrategy == -1 && previousPartnerStrategy == -1)
+    {
+        return;
+    }
+    else
+    {
+        if (previousPlayerStrategy == DECEPTION || previousPartnerStrategy == DECEPTION)
+        {
+            *currentPlayerStrategy = DECEPTION;
+        }
+    }
     return;
 }
 
-void joss (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+void joss(int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
 {
+    double probability = 0.9;
+    if (previousPlayerStrategy == -1 && previousPartnerStrategy == -1)
+    {
+        *currentPlayerStrategy = COOPERATION;
+    }
+    else
+    {
+        if (previousPartnerStrategy == COOPERATION)
+        {
+            if ((double)rand() / RAND_MAX < probability)
+            {
+                *currentPlayerStrategy = COOPERATION;
+            }
+            else
+            {
+                *currentPlayerStrategy = DECEPTION;
+            }
+        }
+        else
+        {
+            *currentPlayerStrategy = DECEPTION;
+        }
+    }
     return;
 }
 
-void random (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+void random(int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
 {
+    double probability = 0.5;
+    if ((double)rand() / RAND_MAX < probability)
+    {
+        *currentPlayerStrategy = COOPERATION;
+    }
+    else
+    {
+        *currentPlayerStrategy = DECEPTION;
+    }
     return;
 }
 
-void allD (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+void allD(int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
 {
+    *currentPlayerStrategy = DECEPTION;
     return;
 }
 
-void allC (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+void allC(int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
 {
+    *currentPlayerStrategy = COOPERATION;
     return;
 }
