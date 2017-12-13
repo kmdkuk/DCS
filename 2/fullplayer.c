d4aef0cd206b0e15aeccad83c0571ea8f230d0e3
kouki <kouworld0123@gmail.com>
2017-12-14 03:12:25 +0900

    wikipediaに載ってるもので実装できそうな戦略の雛形を羅列

---
+++ 2/fullplayer.c
@@ -0,0 +1,48 @@
+void Player1 (int previousPlayer1Strategy, int previousPlayer2Strategy, int *currentPlayer1Strategy)
+{
+    return;
+}
+void Player2 (int previousPlayer1Strategy, int previousPlayer2Strategy, int *currentPlayer2Strategy)
+{
+    return;
+}
+
+void tit4tat (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+{
+    return;
+}
+
+void rtit4tat (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+{
+    return;
+}
+
+void tit42tat (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+{
+    return;
+}
+
+void friedman (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+{
+    return;
+}
+
+void joss (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+{
+    return;
+}
+
+void random (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+{
+    return;
+}
+
+void allD (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+{
+    return;
+}
+
+void allC (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
+{
+    return;
+}
