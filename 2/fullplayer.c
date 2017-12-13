8f7cde3004fa590a6c9d8ccb56f1f16dc1f7ddd0
kouki <kouworld0123@gmail.com>
2017-12-14 04:06:44 +0900

    tit4tat

--- 2/fullplayer.c
+++ 2/fullplayer.c
@@ -1,3 +1,27 @@
+#ifndef STDIO_H
+#define STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef STDLIB_H
+#define STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifndef TIME_H
+#define TIME_H
+#include <time.h>
+#endif
+
+#ifndef COOPERATION
+#define COOPERATION 1
+#endif
+
+#ifndef DECEPTION
+#define DECEPTION 0
+#endif
+
+
 void Player1 (int previousPlayer1Strategy, int previousPlayer2Strategy, int *currentPlayer1Strategy)
 {
     return;
@@ -9,6 +33,13 @@ void Player2 (int previousPlayer1Strategy, int previousPlayer2Strategy, int *cur
 
 void tit4tat (int previousPlayerStrategy, int previousPartnerStrategy, int *currentPlayerStrategy)
 {
+    if (previousPlayerStrategy == -1 && previousPartnerStrategy == -1)
+    {
+        *currentPlayerStrategy = COOPERATION;
+    }else
+    {
+        *currentPlayerStrategy = previousPartnerStrategy;
+    }
     return;
 }
 
