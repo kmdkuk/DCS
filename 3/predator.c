fbe48f21a41f28eb64f05be7bcd9f1d48c9533af
kouki <kouworld0123@gmail.com>
2018-02-07 18:40:59 +0900

    3/型枠をセットアップ2

---
+++ 3/predator.c
@@ -0,0 +1,12 @@
+
+void Predator(int world1[16][16], int world2[16][16], int world3[16][16], int world4[16][16], int* action1, int* action2, int* action3, int* action4)
+{
+
+    return;
+}
+
+void Predator1(int world[16][16], int* action)
+{
+
+    return;
+}
---
+++ 3/prey.c
@@ -0,0 +1,5 @@
+void Prey(int world[16][16], int* Action)
+{
+    
+    return;
+}
